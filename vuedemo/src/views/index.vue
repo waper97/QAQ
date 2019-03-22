bccae500e20a42b288ff1bb50792585adfd4f98f
waper97 <563667426@qq.com>
2019-03-22 18:13:58 +0800

    add vuedemo

---
+++ vuedemo/src/views/index.vue
@@ -0,0 +1,33 @@
+<template>
+      <div>
+        123
+          <el-form>
+            <el-table
+              :data="tableData"
+              border
+              tooltip-effect="light"
+              align="center"
+              style="width: 100%"
+            >
+            </el-table>
+
+          </el-form>
+      </div>
+</template>
+
+<script>
+  name:"test"
+  import {queyUserList} from "@/request/index";
+  export default {
+      name: "index",
+      data(){
+        return{
+          tableData:[]
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
