7da57fd3fa98bfe4c0ed9e5e621665b0171598f9
waper97 <563667426@qq.com>
2021-12-28 18:07:39 +0800

    瞎几把改的

--- springCloud-nacos/nacos-spring-cloud-example/src/main/java/com/example/nacosspringcloudexample/ConfigController.java
+++ springCloud-nacos/nacos-spring-cloud-example/src/main/java/com/example/nacosspringcloudexample/ConfigController.java
@@ -16,20 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class ConfigController {
 
-    @Value("${useLocalCache:false}")
-    private boolean useLocalCache;
-    @Value("${user}")
-    private String user;
-    @Value("${server.port}")
-    private Integer port;
-    @Value("${fuck}")
-    private String fuck;
+//    @Value("${useLocalCache:false}")
+//    private boolean useLocalCache;
+//    @Value("${user}")
+//    private String user;
+//    @Value("${server.port}")
+//    private Integer port;
+//    @Value("${fuck}")
+//    private String fuck;
 
-    @RequestMapping("/get")
-    public boolean get() {
-        System.out.println(user);
-        System.out.println(port);
-        System.out.println(fuck);
-        return useLocalCache;
-    }
+//    @RequestMapping("/get")
+//    public boolean get() {
+//        System.out.println(user);
+//        System.out.println(port);
+//        System.out.println(fuck);
+//        return useLocalCache;
+//    }
 }
--- springCloud-nacos/pazs/src/main/java/com/example/pazs/PazsApplication.java
+++ springCloud-nacos/pazs/src/main/java/com/example/pazs/PazsApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,8 +28,8 @@ public class PazsApplication {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-//    @Value("${user}")
-//    private String name;
+    @Value("${fuck.you}")
+    private String name;
     @RestController
     public class TestController {
 
@@ -40,9 +38,9 @@ public class PazsApplication {
         @Autowired
         public TestController(RestTemplate restTemplate) {this.restTemplate = restTemplate;}
 
-//        @RequestMapping(value = "test", method = RequestMethod.GET)
-//        public String echo() {
-////            return user;
-//        }
+        @RequestMapping(value = "test", method = RequestMethod.GET)
+        public String echo() {
+            return name;
+        }
     }
 }
